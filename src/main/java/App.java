7145214235aa287224032065434e19bb0b993b23
ThomasLump <thomaslump@hotmail.com>
2025-01-03 01:34:49 +0300

    test 2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,12 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean == bean2);
+        Cat cat1 = applicationContext.getBean("cat", Cat.class);
+        System.out.println(cat1.toString());
+        Cat cat2 = applicationContext.getBean("cat", Cat.class);
+        System.out.println(cat2.toString());
+        System.out.println(cat1 == cat2);
     }
 }
